e312da148af760fb6242a2f478222394fa23ecbb
priana13 <herian.sap@gmail.com>
2024-07-24 16:00:19 +0700

    looping

--- src/MenuResto.jsx
+++ src/MenuResto.jsx
@@ -6,17 +6,34 @@ function MenuResto(){
 	const isMakanan = false;
 
 	const isFree = true;
+
+	const menu = [
+		 { nama: 'Nasi Bakar', harga: 'Rp. 10.000'},
+		 { nama: 'Mie Ayam', harga: 'Rp. 15.000'},
+		 { nama: 'Ayam Bakar', harga: 'Rp. 17.000'},
+			
+		];
 	
 
 	return (
-			<div className="menu-item"> 
-				<div>
-					Menu: Nasi Bakar
-				</div>
+			<div> 
+				
 
 				{(isMakanan == true) ? (<div>Makanan</div>) : (<div>Minuman </div>)}
 
 				{(isFree == false) && (<div>Harga: 75.000</div>)}
+
+
+				{
+					menu.map((item , index)=> {
+						return (
+								<div className="menu-item" key="{index}">
+									Menu: {item.nama} - 
+									Harga: {item.harga}
+								</div>
+							)
+					})
+				}
 
 			</div>
 		)
